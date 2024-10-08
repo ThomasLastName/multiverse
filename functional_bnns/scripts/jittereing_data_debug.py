b9a6c1d27cbfbab167fa8b8d951e52c88fbf736f
Thomas (Tom) Winckelman <thomas.k.winckelman@gmail.com>
2024-10-08 14:59:04 -0500

    IDK why it wasn't working before TBH, but I guess it's working now

--- functional_bnns/scripts/jittereing_data_debug.py
+++ functional_bnns/scripts/jittereing_data_debug.py
@@ -1,6 +1,5 @@
 
 import torch
-from bnns.data.slosh_70_15_15 import x_train
+from bnns.data.slosh_70_15_15 import dists
 
-interpolary_grid = x_train + 0.05*torch.randn_like(x_train)
-dists = torch.cdist(interpolary_grid,x_train).min(dim=1).values
+print(dists.unique().shape)
