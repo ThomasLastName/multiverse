edd80bb7a400d09ee537c9259bc4f2eb10859416
Thomas (Tom) Winckelman <thomas.k.winckelman@gmail.com>
2024-10-31 14:13:38 -0500

    Incorporated GP class

--- functional_bnns/bnns/models/slosh_70_15_15_centered_pca_BNN_GP_prior.py
+++ functional_bnns/bnns/models/slosh_70_15_15_centered_pca_BNN_GP_prior.py
@@ -1,10 +1,7 @@
 
 import torch
 from torch import nn
-from bnns.data.slosh_70_15_15_centered_pca import x_test
 from bnns.models.slosh_70_15_15_centered_pca_BNN import BNN
+from bnns.GPR import simple_mean_zero_RPF_kernel_GP as GP
 
-BNN.use_GP_prior = True
-BNN.prior_GP_kernel_bandwidth = torch.tensor(BNN.out_features*[15.])
-BNN.prior_GP_kernel_scale = torch.tensor(BNN.out_features*[1.])
-BNN.prior_GP_kernel_eta = torch.tensor(BNN.out_features*[0.19])
+BNN.GP = GP( out_features=BNN.out_features, eta=0.1 )
