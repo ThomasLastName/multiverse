e1f41dd7bc77996f0f5732fc829b5fe247b77dcf
Thomas (Tom) Winckelman <thomas.k.winckelman@gmail.com>
2024-11-10 17:31:47 -0600

    Uncomment that

--- functional_bnns/bnns/experiments/tuning_loop.py
+++ functional_bnns/bnns/experiments/tuning_loop.py
@@ -34,49 +34,49 @@ sorted_list_of_filenames_starting_with_RUN_THIS = sorted(
     )
 N = int( sorted_list_of_filenames_starting_with_RUN_THIS[-1][len("RUN_THIS_"):].strip(".json") )    # ~~~ e.g., if sorted_list_of_filenames_starting_with_RUN_THIS[-1]=="RUN_THIS_199.json", then N==199
 
-# #
-# # ~~~ For `hours` hours, run the remaining experiments
-# start_time = time()
-# minutes_since_start_time = 0.
-# while minutes_since_start_time*60 < hours:
-#     #
-#     # ~~~ Load the .json file sorted_list_of_filenames_starting_with_RUN_THIS[0]
-#     experiment_filename = sorted_list_of_filenames_starting_with_RUN_THIS.pop(0)
-#     count = int( experiment_filename[len("RUN_THIS_"):].strip(".json") )    # ~~~ e.g., if sorted_list_of_filenames_starting_with_RUN_THIS[-1]=="RUN_THIS_62.json", then count==62
-#     experiment_filename = os.path.join( folder_name, experiment_filename )
-#     hyperparameter_dict = json_to_dict(experiment_filename)
-#     #
-#     # ~~~ Create a new .json file to store the results
-#     tag = generate_json_filename( message=f"EXPERIMENT {count}/{N}" )
-#     result_filename = os.path.join( folder_name, tag )
-#     dict_to_json( hyperparameter_dict, result_filename, verbose=False )
-#     #
-#     # ~~~ Infer which training script to run, based on the hyperparameters
-#     if any( key=="GAUSSIAN_APPROXIMATION" for key in hyperparameter_dict.keys() ):
-#         algorithm = "bnn"
-#     elif any( key=="STEIN" for key in hyperparameter_dict.keys() ):
-#         algorithm = "ensemble"
-#     else:
-#         algorithm = "nn"
-#     #
-#     # ~~~ Run the training script on that dictionary of hyperparameters
-#     command = f"python train_{algorithm}.py --json {result_filename} --overwrite_json"
-#     if save_trained_models:
-#         command += f" --model_save_dir {os.path.join( folder_name, 'experimental_models' )}"
-#     output = run( command, shell=True )
-#     #
-#     # ~~~ Break out of the loop if there was an error in `train_nn.py`
-#     if not output.returncode==0:
-#         break
-#     #
-#     # ~~~ Delete the .json file that prescribed the experiment now run
-#     os.remove(experiment_filename)
-#     #
-#     # ~~~ Record how long we've been at it
-#     minutes_since_start_time = (time()-start_time)/60
-#     #
-#     # ~~~ Perfectionist, superficial formatting of output
-#     print("")
+#
+# ~~~ For `hours` hours, run the remaining experiments
+start_time = time()
+minutes_since_start_time = 0.
+while minutes_since_start_time < hours*60:
+    #
+    # ~~~ Load the .json file sorted_list_of_filenames_starting_with_RUN_THIS[0]
+    experiment_filename = sorted_list_of_filenames_starting_with_RUN_THIS.pop(0)
+    count = int( experiment_filename[len("RUN_THIS_"):].strip(".json") )    # ~~~ e.g., if sorted_list_of_filenames_starting_with_RUN_THIS[-1]=="RUN_THIS_62.json", then count==62
+    experiment_filename = os.path.join( folder_name, experiment_filename )
+    hyperparameter_dict = json_to_dict(experiment_filename)
+    #
+    # ~~~ Create a new .json file to store the results
+    print("")
+    tag = generate_json_filename( message=f"EXPERIMENT {count}/{N}" )
+    print("")
+    result_filename = os.path.join( folder_name, tag )
+    dict_to_json( hyperparameter_dict, result_filename, verbose=False )
+    #
+    # ~~~ Infer which training script to run, based on the hyperparameters
+    if any( key=="GAUSSIAN_APPROXIMATION" for key in hyperparameter_dict.keys() ):
+        algorithm = "bnn"
+    elif any( key=="STEIN" for key in hyperparameter_dict.keys() ):
+        algorithm = "ensemble"
+    else:
+        algorithm = "nn"
+    #
+    # ~~~ Run the training script on that dictionary of hyperparameters
+    command = f"python train_{algorithm}.py --json {result_filename} --overwrite_json"
+    if save_trained_models:
+        command += f" --model_save_dir {os.path.join( folder_name, 'experimental_models' )}"
+    output = run( command, shell=True )
+    #
+    # ~~~ Break out of the loop if there was an error in `train_nn.py`
+    if not output.returncode==0:
+        break
+    #
+    # ~~~ Delete the .json file that prescribed the experiment now run
+    os.remove(experiment_filename)
+    #
+    # ~~~ Record how long we've been at it
+    minutes_since_start_time = (time()-start_time)/60
+
 
 # results = load_filtered_json_files(folder_name)
 # model_mapping = {model: idx for idx, model in enumerate(results["model"].unique())}
