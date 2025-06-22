6b1bd6fc6ff67020a06d90974705ab0eeb2ec377
Thomas (Tom) Winckelman <thomas.k.winckelman@gmail.com>
2025-06-22 15:06:47 -0400

    Remove appendetic models

--- functional_bnns/bnns/models/slosh_70_15_15_centered_pca_BNN_GP_prior.py
+++
@@ -1,7 +0,0 @@
-
-import torch
-from torch import nn
-from bnns.models.slosh_70_15_15_centered_pca_BNN import BNN
-from bnns.GPR import simple_mean_zero_RPF_kernel_GP as GP
-
-BNN.GP = GP( out_features=BNN.out_features, eta=0.1 )
--- functional_bnns/bnns/models/univar_BNN_GP_prior.py
+++
@@ -1,9 +0,0 @@
-
-
-import torch
-from torch import nn
-from bnns.models.univar_BNN import BNN
-from bnns.GPPriorBNN import simple_mean_zero_RPF_kernel_GP as GP
-
-BNN.GP = GP( out_features=BNN.out_features, eta=0.001 )
-
