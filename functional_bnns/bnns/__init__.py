d3503908259bffd4243e055c70dc35d2c52f579f
Thomas (Tom) Winckelman <thomas.k.winckelman@gmail.com>
2025-06-22 16:25:57 -0400

    Try to soft-fail in colab, where there's a dependency conflict in GPyTorch, apparently

--- functional_bnns/bnns/__init__.py
+++ functional_bnns/bnns/__init__.py
@@ -1,13 +1,32 @@
-#
-# ~~~ See distribution
+
 try: from bnns.utils.handling import my_warn
 except: from warnings import warn as my_warn
-from pkg_resources import get_distribution
-dist = get_distribution("bnns")
+
+#
+# ~~~ https://chatgpt.com/share/68586694-1abc-8001-92ac-3db42eef50f6
+try:
+    from pkg_resources import get_distribution
+    __version__ = get_distribution("bnns").version
+except Exception as e:
+    try:
+        from importlib.metadata import version as importlib_version
+    except ImportError:
+        from importlib_metadata import version as importlib_version  # ~~~ for Python <3.8, reportedly
+    try:
+        __version__ = importlib_version("bnns")
+        my_warn(
+            f"Could not retrieve version via pkg_resources due to a possible dependency conflict: {e}"
+        )
+    except Exception as e2:
+        my_warn(
+            f"Could not determine bnns version via pkg_resources or importlib.\n"
+            f"pkg_resources error: {e}\n"
+            f"importlib error: {e2}"
+        )
+        __version__ = "unknown"
 
 #
 # ~~~ Fetch local package version
-__version__ = dist.version
 from .NoPriorBNNs import *
 from .WeightPriorBNNs import *
 from .GPPriorBNNs import *
